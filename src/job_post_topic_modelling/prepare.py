bc0dcc82dfedd7f6c0c91e7b5b21f5c96c4a3b48
AskerNC <hsm467@econ.ku.dk>
2025-09-11 15:13:19 +0200

    cleanup

--- src/job_post_topic_modelling/prepare.py
+++ src/job_post_topic_modelling/prepare.py
@@ -19,7 +19,6 @@ from job_post_topic_modelling.utils.find_project_root import find_project_root  
 if os.environ.get("CONDA_DEFAULT_ENV") in ["job_post_topic_modelling"]:
     user = os.popen("whoami").read().strip()  # noqa: S605, S607
     import nltk  # type: ignore  # noqa: PGH003
-
     nltk.data.path.append(rf"/home/{user}@PROD.SITAD.DK/code/help/installations/nltk_data")
 
 
